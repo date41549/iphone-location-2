241c1398ee836dca27ad90e64a30b6e131652b19
date41549 <hirokidate41549@gmail.com>
2024-05-03 11:57:39 +0900

    Initial Commit

---
+++ iphone-location-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iphone-location-2
+//
+//  Created by Hiroki Date on 2024/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iphone-location-2/iphone_location_2App.swift
@@ -0,0 +1,17 @@
+//
+//  iphone_location_2App.swift
+//  iphone-location-2
+//
+//  Created by Hiroki Date on 2024/05/03.
+//
+
+import SwiftUI
+
+@main
+struct iphone_location_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
